f2194012be12315b79394017b05ea42b45a78f71
Emmanuel <ecoquelin@esensconsulting.com>
2019-05-22 23:03:16 +0200

    masking credentials

--- firmware/provision-wework.bat
+++ firmware/provision-wework.bat
@@ -1,4 +1,4 @@
 mos build --platform esp32
 mos flash
-mos wifi "WeWork" P@ssw0rd
-mos gcp-iot-setup --gcp-project iot-flex-pad --gcp-region europe-west1 --gcp-registry flex-pad-registry-0
+mos wifi "<SSID>" <Password>
+mos gcp-iot-setup --gcp-project <project-name> --gcp-region europe-west1 --gcp-registry flex-pad-registry-0
